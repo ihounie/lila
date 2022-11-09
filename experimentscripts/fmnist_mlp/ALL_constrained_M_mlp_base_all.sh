382bec0348b9544ad94217bedc08d704d9426dc1
ihounie <ihounie@seas.iupenn.edu>
2022-11-09 10:50:30 -0500

    sensitivity scripts

--- experimentscripts/fmnist_mlp/ALL_constrained_M_mlp_base_all.sh
+++ experimentscripts/fmnist_mlp/ALL_constrained_M_mlp_base_all.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-EPS=0.5
 LRDUAL=0.0005
 EPOCHS=400
 DEVICE="cuda:1"
 for seed in 1 2 3
-do
+do  
+    EPS=0.8
+    for model in "mlp"
+        do
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset translated_fmnist --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset fmnist_r180 --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset fmnist_r90 --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset scaled_fmnist --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        done
+    done
+    EPS=0.6
     for model in "cnn"
         do
-        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset translated_mnist --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
-        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset mnist_r180 --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
-        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset mnist_r90 --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
-        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset scaled_mnist --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset translated_fmnist --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset fmnist_r180 --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset fmnist_r90 --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
+        python classification_image.py --lr_dual $LRDUAL --epsilon $EPS --model $model --method constrained --dataset scaled_fmnist --n_epochs $EPOCHS --device $DEVICE --n_samples_aug 2 --save --batch_size 1000 --seed $seed
         done
     done
 done
